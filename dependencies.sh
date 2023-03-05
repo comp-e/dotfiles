41e8e179859d2376faac8c5ec3a6920648acb87e
comp-e <alexismastakouris08@gmail.com>
2023-03-05 12:26:05 +0200

    dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -1,5 +1,5 @@
 # --- Xorg ---
-doas pacman -S --needed xorg xorg-server xorg-apps xorg-xinit libx11 libxft libxinerama webgtk2
+doas pacman -S --needed xorg xorg-server xorg-apps xorg-xinit libx11 libxft libxinerama webkit2gtk
 
 # --- Utilities ---
 doas pacman -S --needed vim neovim picom sxhkd htop wget curl feh nitrogen pulseaudio pulsemixer pamixer 
